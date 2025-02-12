3b64869096d9a69c634866bb59f314022c3bae2b
MuhammedMagdyy <mohamedmody04@gmail.com>
2025-02-12 13:25:10 +0200

    feat: add XSS protection middleware and email verification check middleware

---
+++ src/middlewares/attacks.middleware.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from 'express';
+import { JSDOM } from 'jsdom';
+import createDOMPurify from 'dompurify';
+
+const { window } = new JSDOM('');
+const DOMPurify = createDOMPurify(window);
+
+export const xss = (req: Request, res: Response, next: NextFunction) => {
+  for (const key in req.body) {
+    if (typeof req.body[key] === 'string') {
+      req.body[key] = DOMPurify.sanitize(req.body[key], { ALLOWED_TAGS: [] });
+    }
+  }
+  next();
+};
--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,3 +1,5 @@
 export * from './error-handler.middleware';
 export * from './isAuth.middleware';
 export * from './multer.middleware';
+export * from './isVerified.middleware';
+export * from './attacks.middleware';
