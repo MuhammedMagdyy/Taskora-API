f65830a3a26a32f18f2fb5fb5e225ecd9ddf08d0
MuhammedMagdyy <mohamedmody04@gmail.com>
2025-02-25 01:04:38 +0200

    refactor: made description nullable

--- src/utils/validations/tasks.ts
+++ src/utils/validations/tasks.ts
@@ -2,11 +2,7 @@ import { z } from 'zod';
 
 export const taskSchema = z.object({
   name: z.string().min(1, 'Name must be at least 1 character').trim(),
-  description: z
-    .string()
-    .min(1, 'Description must be at least 1 characters')
-    .trim()
-    .optional(),
+  description: z.string().trim().optional(),
   dueDate: z.coerce.date().optional(),
   projectUuid: z.string().uuid({ message: 'project is required' }),
   tagUuid: z.string().uuid().optional(),
@@ -16,11 +12,7 @@ export const taskSchema = z.object({
 export const taskUpdateSchema = z
   .object({
     name: z.string().min(1, 'Name must be at least 1 character').trim(),
-    description: z
-      .string()
-      .min(1, 'Description must be at least 1 characters')
-      .trim()
-      .optional(),
+    description: z.string().trim().optional(),
     dueDate: z.coerce.date({ message: 'Due date is required' }),
     projectUuid: z.string().uuid({ message: 'Project is required' }),
     tagUuid: z.string().uuid({ message: 'Tag is required' }),
